61950a48c43c923036d12d673451cd1c6735a56b
Ashraful Islam Emon <55305586+ashrafulIslam1@users.noreply.github.com>
2023-01-29 17:30:02 +0600

    Update the Category display name, categoryName to Category Name.

--- Expense Tracker/ViewModels/expenseCategoryViewModel.cs
+++ Expense Tracker/ViewModels/expenseCategoryViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Expense_Tracker.Controllers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Expense_Tracker.ViewModels
 {
@@ -7,6 +8,7 @@ namespace Expense_Tracker.ViewModels
     {
         public int categoryID { get; set; }
         [Remote("IsCategoryNameExist", "expenseCategory", ErrorMessage = "This category name is already available")]
+        [Display(Name = "Category Name")]
         public string categoryName { get; set; }
     }
 }
--- Expense Tracker/ViewModels/expensesViewModel.cs
+++ Expense Tracker/ViewModels/expensesViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Expense_Tracker.Daterange_Attribute;
+using System.ComponentModel.DataAnnotations;
 
 namespace Expense_Tracker.ViewModels
 {
@@ -6,6 +7,7 @@ namespace Expense_Tracker.ViewModels
     {
         public int Id { get; set; }
         public int categoryId { get; set; }
+        [Display(Name = "Category Name")]
         public string? categoryName { get; set; }
         [CurrentDate(ErrorMessage = "Invalid date, please give current date.")]
         public DateTime Date { get; set; }
